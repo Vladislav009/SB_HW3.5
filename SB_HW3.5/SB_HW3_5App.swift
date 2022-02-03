e96c6ecb097d71862fa487bba502a5d284d8a572
Vladislav <developer@arthurandco.org>
2022-02-03 17:44:42 +0500

    Initial Commit

---
+++ SB_HW3.5/SB_HW3_5App.swift
@@ -0,0 +1,17 @@
+//
+//  SB_HW3_5App.swift
+//  SB_HW3.5
+//
+//  Created by Vladislav Kulak on 03.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SB_HW3_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
